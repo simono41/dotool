41be5e5e4f1448487ee97cd43d1214471c542343
John Gebbie <me@johngebbie.com>
2023-01-10 10:41:45 +0000

    install: let $DOTOOL_VERSION set --version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # ./install.sh [DESTDIR] [BINDIR]
-version="$(git describe --long --abbrev=12 --tags --dirty 2>/dev/null || echo 1.0)"
-go build -ldflags "-X main.Version=$version" || exit
+: "${DOTOOL_VERSION=$(git describe --long --abbrev=12 --tags --dirty 2>/dev/null || echo 1.0)}"
+go build -ldflags "-X main.Version=$DOTOOL_VERSION" || exit
 mkdir -p "$1/${2:-usr/local/bin}" || exit
 cp -v dotool dotoolc dotoold "$1/${2:-usr/local/bin}" || exit
 mkdir -p "$1/etc/udev/rules.d" || exit
